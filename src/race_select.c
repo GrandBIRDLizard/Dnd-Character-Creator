d56b812d3b03008521ca83a73e740ecc3c18d901
GrandBIRDLizard <justin.munch9@gmail.com>
2025-08-24 19:42:39 -0400

    Reformat race_select.c, text, UI

--- src/race_select.c
+++ src/race_select.c
@@ -15,13 +15,13 @@ PageNum RaceSelect(struct Player *player) {
 	const char *dragonTypes = "Black;Blue;Brass;Bronze;Copper;Gold;Green;Red;Silver;White";
 	const char *elvenLineages = "Drow;High Elf;Wood Elf"; 
 
-    GuiLabel((Rectangle){10, 40, 200, 20}, "Choose Option:");
+    GuiLabel((Rectangle){10, 15, 200, 20}, "Choose Option:");
 
     if (GuiButton((Rectangle){680, 750, 100, 30}, "Next")) {
         return PAGE2;
     }
 
-    if (GuiButton((Rectangle){10, 120, 100, 30}, "Confirm Race")) {
+    if (GuiButton((Rectangle){220, 40, 110, 30}, "Confirm Race")) {
         switch (optionSelected) {
             case 0: strcpy(player->Race, "Dragonborn"); break;
             case 1: strcpy(player->Race, "Dwarf"); break;
@@ -36,9 +36,9 @@ PageNum RaceSelect(struct Player *player) {
     }
 	
 	if (strcmp(player->Race, "Dragonborn") == 0) {
-		DrawText(TextFormat("Your lineage stems from a dragon progenitor. Choose the kind of dragon from the Draconic Ancestors table.\nYour choice affects your Breath Weapon and Damage Resistance traits as well as your appearance."), 10, 300, 14, RAYWHITE);
-		GuiLabel((Rectangle){10, 350, 200, 20}, "Choose Type:");
-		if (GuiButton((Rectangle){10, 430, 100, 30}, "Confirm Type ")) { 
+		DrawText(TextFormat("Your lineage stems from a dragon progenitor.\nChoose the kind of dragon from the Draconic Ancestors table.\nYour choice affects your Breath Weapon,\nDamage Resistance traits, and your appearance."), 10, 165, 20, RAYWHITE);
+		GuiLabel((Rectangle){10, 310, 200, 20}, "Choose Type:");
+		if (GuiButton((Rectangle){220, 335, 110, 30}, " Confirm Type ")) { 
 			switch (typeSelected) {
 				case 0: 
 					strcpy(player->DragonType, "Black");
@@ -82,48 +82,48 @@ PageNum RaceSelect(struct Player *player) {
 					break;
 			}
 		}
-	DrawText(TextFormat("Type selected: %s", player->DragonType), 10, 470, 20, RAYWHITE);
+	DrawText(TextFormat("Type selected: %s", player->DragonType), 10, 385, 20, YELLOW);
 
-	DrawText(TextFormat("Damage type: %s", player->DamageType), 10, 495, 20, RAYWHITE);
+	DrawText(TextFormat("Damage type: %s", player->DamageType), 10, 415, 20, YELLOW);
 
-	if (GuiDropdownBox((Rectangle){10, 380, 200, 30}, dragonTypes, &typeSelected, dropTypesActive)) {
+	if (GuiDropdownBox((Rectangle){10, 335, 200, 30}, dragonTypes, &typeSelected, dropTypesActive)) {
 		dropTypesActive = false;
 	}
 	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
-		CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 380, 200, 30})) {
+		CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 335, 200, 30})) {
 		dropTypesActive = !dropTypesActive;
 	}	
 	}
 
 	if (strcmp(player->Race, "Elf") == 0) {
-		DrawText(TextFormat("You are part of a lineage that grants you supernatural abilities. Choose a lineage from the Elven Lineages table. You gain the level 1 benefit of that lineage."), 10, 300, 14, RAYWHITE);
-		GuiLabel((Rectangle){10, 350, 200, 20}, "Choose Lineage:");
-		if (GuiButton((Rectangle){10, 430, 100, 30}, "Confirm Lineage ")) {
+		DrawText(TextFormat("You are part of a lineage that grants you supernatural abilities.\nChoose a lineage from the Elven Lineages table.\nYou gain the level 1 benefit of that lineage."), 10, 170, 20, RAYWHITE);
+		GuiLabel((Rectangle){10, 310, 200, 20}, "Choose Lineage:");
+		if (GuiButton((Rectangle){220, 335, 110, 30}, " Confirm Lineage ")) {
 			switch (lineageSelected) {
 				case 0: strcpy(player->Lineage, "Drow"); break;
 				case 1: strcpy(player->Lineage, "High Elf"); break;
 				case 2: strcpy(player->Lineage, "Wood Elf"); break;
 			}
 		}
-	DrawText(TextFormat("Lineage Selected: %s", player->Lineage), 10, 470, 20, RAYWHITE);
+	DrawText(TextFormat("Lineage Selected: %s", player->Lineage), 10, 385, 20, YELLOW);
 
-	if (GuiDropdownBox((Rectangle){10, 380, 200, 30}, elvenLineages, &lineageSelected, dropLineageActive)) {
+	if (GuiDropdownBox((Rectangle){10, 335, 200, 30}, elvenLineages, &lineageSelected, dropLineageActive)) {
 		dropLineageActive = false;
 	}
 	if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
-		CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 380, 200, 30})) {
+		CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 335, 200, 30})) {
 		dropLineageActive = !dropLineageActive;
 	}
 	}
 
 
-    DrawText(TextFormat("Option selected: %s", player->Race), 10, 160, 20, RAYWHITE);
+    DrawText(TextFormat("Option selected: %s", player->Race), 10, 90, 20, YELLOW);
 
-    if (GuiDropdownBox((Rectangle){10, 70, 200, 30}, raceOptions, &optionSelected, dropOptionsActive)) {
+    if (GuiDropdownBox((Rectangle){10, 40, 200, 30}, raceOptions, &optionSelected, dropOptionsActive)) {
         dropOptionsActive = false;
     }
     if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON) &&
-        CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 70, 200, 30})) {
+        CheckCollisionPointRec(GetMousePosition(), (Rectangle){10, 40, 200, 30})) {
         dropOptionsActive = !dropOptionsActive;
     }
 
